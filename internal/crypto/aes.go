1e296118cbe41796ad9bd49f7caeaa82ee734547
apokryptein <6676998+apokryptein@users.noreply.github.com>
2025-04-23 14:59:31 -0400

    feat: set2/challenge10
    AES CBC Decryption

--- internal/crypto/aes.go
+++ internal/crypto/aes.go
@@ -2,8 +2,9 @@ package crypto
 
 import (
 	"crypto/aes"
-	"crypto/cipher"
 	"fmt"
+
+	"github.com/apokryptein/cryptopals-go/internal/encoding"
 )
 
 func DecryptAES_ECB(key, data []byte) ([]byte, error) {
@@ -59,10 +60,7 @@ func DetectAES_ECB(data []byte, blockSize int) bool {
 }
 
 func DecryptAES_CBC(key, iv, data []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, fmt.Errorf("error creating new cipher: %w", err)
-	}
+	var err error
 
 	// Check ciphertext is of sufficient length
 	if len(data) < aes.BlockSize {
@@ -70,15 +68,39 @@ func DecryptAES_CBC(key, iv, data []byte) ([]byte, error) {
 	}
 
 	// Ensure ciphertext is multiple of blocksize
+	// If not, pad with PKCS7
 	if len(data)%aes.BlockSize != 0 {
-		return nil, fmt.Errorf("ciphertext is not a multiple of blocksize")
+		data, err = PaddingPKCS7(data, aes.BlockSize)
+		if err != nil {
+			return nil, fmt.Errorf("error padding data: %w", err)
+		}
 	}
 
-	iv = data[:aes.BlockSize]
-	// data = data[aes.BlockSize:]
-	mode := cipher.NewCBCDecrypter(block, iv)
+	previousBlock := iv
+	plaintext := make([]byte, len(data))
 
-	mode.CryptBlocks(data, data)
+	for i := 0; i < len(data); i += aes.BlockSize {
+		// Get current block
+		currentBlock := data[i : i+aes.BlockSize]
 
-	return data, nil
+		// Decrypt current block
+		decData, err := DecryptAES_ECB(key, currentBlock)
+		if err != nil {
+			return nil, fmt.Errorf("error decrypting block: %w", err)
+		}
+
+		// XOR with encrypted previous block (or IV) to get plaintext
+		p, err := encoding.FixedXOR(decData, previousBlock)
+		if err != nil {
+			return nil, fmt.Errorf("error xoring block: %w", err)
+		}
+
+		// previousBlock is current endcrypted block
+		previousBlock = currentBlock
+
+		// Append decrypted block to plaintext
+		plaintext = append(plaintext, p...)
+	}
+
+	return plaintext, nil
 }
