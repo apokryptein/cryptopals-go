76def8b4bb320b7a54bdedf95bd920e108a9b757
apokryptein <6676998+apokryptein@users.noreply.github.com>
2025-07-07 12:19:56 -0400

    (feat): challenge 13 completed

--- internal/set2/challenge13.go
+++ internal/set2/challenge13.go
@@ -5,44 +5,80 @@ import (
 	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"strings"
 
+	"github.com/apokryptein/cryptopals-go/internal/crypto"
 	"github.com/apokryptein/cryptopals-go/internal/encoding"
 )
 
-func Challenge13() error {
+func Challenge13() (*encoding.Profile, error) {
 	// UID generator
 	uidGen := &encoding.UIDGen{ID: 10}
 
 	// Generate new profile from email
 	profile, err := encoding.NewProfile("foo@bar.com", uidGen)
 	if err != nil {
-		return fmt.Errorf("failed to create new profile: %w", err)
+		return nil, fmt.Errorf("failed to create new profile: %w", err)
 	}
 
 	// Marshal
 	jsonPro, err := json.MarshalIndent(profile, "", " ")
 	if err != nil {
-		return fmt.Errorf("failed to marshal profile: %w", err)
+		return nil, fmt.Errorf("failed to marshal profile: %w", err)
 	}
 
 	// DEBUG
-	fmt.Printf("New profile:\n%s\n", string(jsonPro))
+	fmt.Printf("=== New Original Profile ===\n%s\n", string(jsonPro))
 
-	// Conver to encoded cookie format
+	// Convert to encoded cookie format
 	proCookie := profile.ProfileToCookie()
-	fmt.Printf("Profile cookie: %s\n", proCookie)
+	fmt.Printf("Original Profile Cookie => %s\n\n", proCookie)
 
 	// Generate random 16-byte key
 	key := make([]byte, aes.BlockSize)
 	if _, err := rand.Read(key); err != nil {
-		return fmt.Errorf("key gen: %w", err)
+		return nil, fmt.Errorf("key gen: %w", err)
 	}
 
-	// TODO:
-	// 1. encrypt profiles only differing by group (user vs. admin)
-	// 2. Identify the block holding the user group data
-	// 3. Replace the data in encrypted profile
-	// 4. Decrypt with key and compare
+	// Set up admin cookie to check encrypted block differences
+	adminCookie := "email=foo@bar.com&uid=10&role=admin"
 
-	return nil
+	// Encrypt valid profile cookie
+	proEnc, err := crypto.EncryptAESECB(key, []byte(proCookie))
+	if err != nil {
+		return nil, fmt.Errorf("failed to encrypt: %w", err)
+	}
+
+	// Encrypt malicious profile cookie
+	adminEnc, err := crypto.EncryptAESECB(key, []byte(adminCookie))
+	if err != nil {
+		return nil, fmt.Errorf("failed to encrypt: %w", err)
+	}
+
+	// Construct forged cookie
+	forgedCookie := proEnc[:16]
+	forgedCookie = append(forgedCookie, adminEnc[16:]...)
+
+	// Decrypte forged cookie
+	forgedDec, err := crypto.DecryptAESECB(key, forgedCookie)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decrypt: %w", err)
+	}
+
+	// Clean up string
+	cleanForged := strings.TrimSpace(string(forgedDec))
+
+	// Create new profile from cookie
+	forgedProfile := &encoding.Profile{}
+	forgedProfile.ProfileFromCookie(cleanForged)
+
+	jsonForged, err := json.MarshalIndent(forgedProfile, "", " ")
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal profile: %w", err)
+	}
+
+	// DEBUG
+	fmt.Printf("=== Forged Profile ===\n%s\n\n", string(jsonForged))
+
+	return forgedProfile, nil
 }
--- internal/set2/challenge13_test.go
+++ internal/set2/challenge13_test.go
@@ -1,7 +1,26 @@
 package set2_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/apokryptein/cryptopals-go/internal/set2"
+)
 
 func TestChallenge13(t *testing.T) {
-	// TODO: do this
+	// Run Challeng13 and get resululting profile
+	forgedProfile, err := set2.Challenge13()
+	if err != nil {
+		t.Errorf("failure running Challenge 13: %v", err)
+	}
+
+	// Profile cookie that we want
+	want := "email=foo@bar.com&uid=10&role=admin"
+
+	// Profile cookit that we got
+	have := forgedProfile.ProfileToCookie()
+
+	// Test
+	if want != have {
+		t.Errorf("Wanted: %s\nHave: %s\n", want, have)
+	}
 }
